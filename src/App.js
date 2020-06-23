9ac7a74faec5b65697c90565ce6acbe51faad7a3
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-23 12:34:31 -0500

    Obteniendo y mostrando la letra de la cancion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import axios from "axios";
 
 function App() {
+  //state
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+
+  const [letra, guardarLetra] = useState("");
+
+  useEffect(() => {
+    if (Object.keys(busquedaletra).length === 0) return;
+
+    const consultarAPILetra = async () => {
+      const { artista, cancion } = busquedaletra;
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      const resultado = await axios(url);
+      guardarLetra(resultado.data.lyrics);
+    };
+
+    consultarAPILetra();
+  }, [busquedaletra]);
+
   return (
     <Fragment>
-      <Formulario></Formulario>
+      <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Formulario = () => {
+const Formulario = ({ guardarBusquedaLetra }) => {
   const [busqueda, guardarBusqueda] = useState({
     artista: "",
     cancion: "",
@@ -22,14 +22,23 @@ const Formulario = () => {
   const buscarInformacion = (e) => {
     e.preventDefault();
 
-    if (artista.trim === "" || artista.trim === "") {
+    if (artista.trim() === "" || cancion.trim() === "") {
       guardarError(true);
+      return;
     }
     guardarError(false);
+
+    //pasar state al compoente principal
+    guardarBusquedaLetra(busqueda);
   };
 
   return (
     <div className="bg-info">
+      {error ? (
+        <p className="alert alert-danger text-center p-2">
+          Todos los campos son obligatorios
+        </p>
+      ) : null}
       <div className="container">
         <div className="row">
           <form
